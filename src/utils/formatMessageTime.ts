9ef40ac3ee85848782297dd6aa3df8c76ac0ba52
KeerthanaVanga <keerthanavangaa@gmail.com>
2026-04-24 13:43:39 +0530

    feat: add utility function to format timestamps to IST string format

--- src/utils/formatMessageTime.ts
+++ src/utils/formatMessageTime.ts
@@ -1,40 +1,14 @@
 export const formatMessageTime = (dateString: string) => {
-  const date = new Date(dateString);
-  const now = new Date();
+  
+  const istTime = new Date(dateString).toLocaleString("en-IN", {
+  timeZone: "Asia/Kolkata",
+  year: "numeric",
+  month: "long",
+  day: "numeric",
+  hour: "2-digit",
+  minute: "2-digit",
+  second: "2-digit",
+});
 
-  const today = new Date(now.getFullYear(), now.getMonth(), now.getDate());
-
-  const yesterday = new Date(
-    now.getFullYear(),
-    now.getMonth(),
-    now.getDate() - 1,
-  );
-
-  const messageDay = new Date(
-    date.getFullYear(),
-    date.getMonth(),
-    date.getDate(),
-  );
-
-  const time = date.toLocaleTimeString("en-IN", {
-    hour: "2-digit",
-    minute: "2-digit",
-    hour12: true,
-  });
-
-  if (messageDay.getTime() === today.getTime()) {
-    return `Today, ${time}`;
-  }
-
-  if (messageDay.getTime() === yesterday.getTime()) {
-    return `Yesterday, ${time}`;
-  }
-
-  const fullDate = date.toLocaleDateString("en-IN", {
-    day: "2-digit",
-    month: "short",
-    year: "numeric",
-  });
-
-  return `${fullDate}, ${time}`;
+  return istTime;
 };
