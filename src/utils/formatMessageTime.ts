ce8e6adcffac0467033648113bbf3e4ff559ab82
KeerthanaVanga <keerthanavangaa@gmail.com>
2026-04-24 19:55:14 +0530

    feat: add formatMessageTime utility for localized timestamp display

--- src/utils/formatMessageTime.ts
+++ src/utils/formatMessageTime.ts
@@ -1,14 +1,44 @@
 export const formatMessageTime = (dateString: string) => {
-  
-  const istTime = new Date(dateString).toLocaleString("en-IN", {
-  timeZone: "Asia/Kolkata",
-  year: "numeric",
-  month: "long",
-  day: "numeric",
-  hour: "2-digit",
-  minute: "2-digit",
-  second: "2-digit",
-});
+  // Convert to ISO format
+  const formatted = dateString.replace(" ", "T").split(".")[0];
 
-  return istTime;
+  const date = new Date(formatted);
+  const now = new Date();
+
+  const today = new Date(now.getFullYear(), now.getMonth(), now.getDate());
+  const yesterday = new Date(
+    now.getFullYear(),
+    now.getMonth(),
+    now.getDate() - 1
+  );
+
+  const messageDay = new Date(
+    date.getFullYear(),
+    date.getMonth(),
+    date.getDate()
+  );
+
+  const time = date.toLocaleTimeString("en-IN", {
+    hour: "2-digit",
+    minute: "2-digit",
+    hour12: true,
+    timeZone: "Asia/Kolkata", // 👈 important
+  });
+
+  if (messageDay.getTime() === today.getTime()) {
+    return `Today, ${time}`;
+  }
+
+  if (messageDay.getTime() === yesterday.getTime()) {
+    return `Yesterday, ${time}`;
+  }
+
+  const fullDate = date.toLocaleDateString("en-IN", {
+    day: "2-digit",
+    month: "short",
+    year: "numeric",
+    timeZone: "Asia/Kolkata", // 👈 important
+  });
+
+  return `${fullDate}, ${time}`;
 };
